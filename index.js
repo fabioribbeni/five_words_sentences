165fdda6b619535ab3238925b511ed44cb17f93e
fabioribbeni <fabio.ribbeni@libero.it>
2021-02-22 14:36:43 +0100

    Creata la funzione per controllare la lunghezza della frase, completate tutte le altre consegne.

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const readline = require('readline');
 const consola = require('consola');
 
+function words_check() {
 
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Please insert a five words sentence ', (answer) => {
+        let wordCount = answer.split(' ');
+
+        if (wordCount.length >= 5) {
+            console.log(`La frase è abbastanza lunga: ${answer}`);
+            rl.close();
+
+        } else {
+            consola.error('La frase contiene meno di 5 parole');
+            words_check();
+        }
+    });
+}
+words_check();
